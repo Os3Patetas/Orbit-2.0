d681efff5010d73765a088c89e97f240a34419eb
gvrDev <guilhermevrodrigues123@gmail.com>
2022-11-16 12:58:20 -0300

    Link Store coins and cash with local value

--- Assets/_Projeto/_Scenes/SelectSkin.cs
+++ Assets/_Projeto/_Scenes/SelectSkin.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using com.icypeak.data;
 
 namespace com.Icypeak.Orbit.Scene
 {
     public class SelectSkin : MonoBehaviour
     {
+        [SerializeField] TextMeshProUGUI cashTextEl;
+        [SerializeField] TextMeshProUGUI coinsTextEl;
+
+        void Start()
+        {
+            cashTextEl.text = LocalDataManager.Instance.CurrencyDataResource.Cash.ToString();
+            coinsTextEl.text = LocalDataManager.Instance.CurrencyDataResource.Coins.ToString();
+        }
+
         public void GoToMainMenu()
         {
             SceneManager.LoadScene("MainMenu");
